23f4fcfc826c8c74bc6ebe20440f6595c50819ec
Tong Hui <tonghuix@gmail.com>
2016-10-10 01:06:26 +0800

    change fonts

--- install.latex.ubuntu.sh
+++ install.latex.ubuntu.sh
@@ -5,5 +5,5 @@ sudo apt-get -y install ttf-wqy-microhei ttf-wqy-zenhei # from WenQuanYi
 sudo apt-get -y install texlive-fonts-recommended
 sudo apt-get -y install latex-cjk-all  fonts-liberation ttf-liberation latex-cjk-chinese 
 sudo apt-get -y install fonts-arphic-gkai00mp fonts-arphic-ukai latex-cjk-chinese-arphic-gkai00mp #install Kaiti font
-sudo apt-get -y install fonts-arphic-gbsn00lp latex-cjk-chinese-arphic-gbsn00lp 
+sudo apt-get -y install fonts-arphic-gbsn00lp latex-cjk-chinese-arphic-gbsn00lp fonts-noto-cjk
 sudo fc-cache
