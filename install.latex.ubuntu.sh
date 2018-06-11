9f3f923367f4ea9555063b710ec8196fac2a623c
郭俊余 <hagbgreen@yahoo.com>
2018-06-11 21:00:24 +0800

    fonts-noto-hinted

--- install.latex.ubuntu.sh
+++ install.latex.ubuntu.sh
@@ -1,5 +1,6 @@
 sudo apt-get -y install texlive-xetex texlive-latex-recommended \
 	texlive-latex-extra texlive-lang-cjk texlive-fonts-recommended \
 	latex-cjk-chinese fonts-liberation latex-cjk-chinese fonts-noto-cjk \
-	librsvg2-bin pdf2svg fonts-noto-hinted #fonts-noto
+	librsvg2-bin pdf2svg fonts-noto
+sudo apt-get -y install fonts-noto-hinted 
 sudo fc-cache
