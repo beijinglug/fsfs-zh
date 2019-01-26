6e4b2d3f97926e73f9f5c512ce59844774a1356b
Iru Cai <mytbk920423@gmail.com>
2019-01-26 20:07:28 +0800

    travis: use addon apt to install package

--- install.latex.ubuntu.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get -y install texlive-xetex texlive-latex-recommended \
-	texlive-latex-extra texlive-lang-cjk texlive-fonts-recommended \
-	latex-cjk-chinese fonts-liberation latex-cjk-chinese fonts-noto-cjk \
-	librsvg2-bin pdf2svg fonts-noto
-sudo fc-cache
